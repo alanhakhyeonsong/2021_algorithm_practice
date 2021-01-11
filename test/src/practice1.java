7e90a8907bb7480f04eae4f529f1ec128e882f8c
alanhakhyeonsong <songs4805@naver.com>
2021-01-11 21:47:47 +0900

    2021_algorithm_practice

---
+++ test/src/practice1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class practice1 {
+	public static void main(String[] args) {
+		int N;
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		System.out.println(N);
+		sc.close();
+	}
+}
+// 환경 설정 셋팅 확인
