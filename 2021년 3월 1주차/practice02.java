342a080ac6a68cf3811da566f7b26c69052a6218
alanhakhyeonsong <songs4805@naver.com>
2021-03-06 04:04:12 +0900

    2021_algorithm_practice

---
+++ 2021년 3월 1주차/practice02.java
@@ -0,0 +1,23 @@
+// BOJ 11047번: 동전 0
+import java.io.*;
+import java.util.StringTokenizer;
+public class practice02 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] coins = new int[N];
+		for(int i = 0; i < N; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
+		int count = 0;
+		for(int i = coins.length - 1; i >= 0; i--) {
+			if(coins[i] <= K) {
+				count += (K / coins[i]);
+				K %= coins[i];
+			}	
+		}
+		System.out.println(count);
+	}
+}
